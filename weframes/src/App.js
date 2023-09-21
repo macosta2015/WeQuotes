e87750e5863429814c3f16507e22ab5a90f985f0
Mario Acosta <macosta2015@my.fit.edu>
2023-09-21 15:42:04 -0400

    Lesson learned. Do not have multiple node_modules!

--- weframes/src/App.js
+++ weframes/src/App.js
@@ -1,8 +1,16 @@
+import React, { createContext, useContext } from 'react';
+import Form from './Form/Form';
+import MediaCard from './Form/MediaCard';
+import ImgMediaCard from './Form/Card'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           Hello WeQuotes
+          <Form />
+          <MediaCard/>
+          <ImgMediaCard/>
       </header>
     </div>
   );
