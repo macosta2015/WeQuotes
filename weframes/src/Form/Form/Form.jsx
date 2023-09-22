b250997efdee903cfec22d43ffa983244cf8b36a
Mario Acosta <macosta2015@my.fit.edu>
2023-09-22 17:16:24 -0400

    Adding the responsive needs in the Form.

--- weframes/src/Form/Form/Form.jsx
+++ weframes/src/Form/Form/Form.jsx
@@ -35,15 +35,18 @@ const Form = () => {
 
   return (
     <Box className="custom-box" sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
+      <Grid container spacing={2}> 
+      {/* Grid spacing is the space columns distance */}
+        <Grid item xs={12}>
+        <Typography variant="h4" className="centered-text">
+          Fill out the Form
+        </Typography>
         </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
+        <Grid item xs={6}>
+          <Item>xs=6</Item>
         </Grid>
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
+        <Grid item xs={6}>
+          <Item>xs=6</Item>
         </Grid>
         <Grid item xs={8}>
           <Item>xs=8</Item>
