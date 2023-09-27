be88be206d9c9865c835bc8adaa93b4427ea1436
Mario Acosta <macosta2015@my.fit.edu>
2023-09-27 19:28:14 -0400

    Update dataExport.jsx

--- weframes/src/Form/Form/dataExport.jsx
+++ weframes/src/Form/Form/dataExport.jsx
@@ -20,4 +20,19 @@ export const exportFormData = (formData) => {
   console.log(formData.name);
 
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  console.log('After running!');
+
+
+
+
 };
