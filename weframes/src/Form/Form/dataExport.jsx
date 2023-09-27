4fa4f77ce1a76eb4a87d7621925eea1cb67c2951
Mario Acosta <macosta2015@my.fit.edu>
2023-09-27 19:24:58 -0400

    commit

--- weframes/src/Form/Form/dataExport.jsx
+++ weframes/src/Form/Form/dataExport.jsx
@@ -1,48 +1,23 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 
-const ExportForm = () => {
-  console.log('Are we running? Inside ExportForm');
+const YOUR_SERVICE_ID = 'service_v156l77'; 
+const YOUR_TEMPLATE_ID = 'template_prjss03'; 
+const YOUR_PUBLIC_KEY = 'nhzh-yC3-sBQXybN0'; 
 
-  const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+const to_name='Mario'
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-  };
+const from_name='Mario'
 
-  return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
-};
+const message='Mario'
+
+const reply_to='Mario'
 
 export const exportFormData = (formData) => {
   console.log('Are we running!');
   console.log('Form data exported:', formData);
   console.log(formData.name);
 
-  // Render the ExportForm component here
 
-
-  return (
-    <div>
-      <ExportForm />
-      {console.log('After rendering!')}
-    </div>
-    
-  );
 };
