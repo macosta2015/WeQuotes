0f2303563daf08eeae643f14a69e868bf70c74dc
Mario Acosta <macosta2015@my.fit.edu>
2023-09-29 16:20:21 -0400

    Code running with the correct dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { createContext, useContext } from 'react';
-import MediaCard from '../Form/MediaCard/MediaCard';
-import Card from '../Form/Card/Card';
-import Header from '../Form/Header/Header';
-import Form from '../Form/Form/Form';
-import { FormDataProvider } from '../FormDataContext'; // Import the FormDataProvider
-import DisplayData from '../Form/Emailoutput/Emailoutput';
+import MediaCard from './Form/MediaCard/MediaCard';
+import Card from './Form/Card/Card';
+import Header from './Form/Header/Header';
+import Form from './Form/Form/Form';
+import { FormDataProvider } from './FormDataContext'; // Import the FormDataProvider
+import DisplayData from './Form/Emailoutput/Emailoutput';
 
 function App() {
   return (
