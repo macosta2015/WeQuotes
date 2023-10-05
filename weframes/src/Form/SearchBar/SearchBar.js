11847ed68d297fca5948bcb763126b1d70c3ddaa
macosta2015 <macosta2015@my.fit.edu>
2023-10-04 23:52:37 -0400

    Update SearchBar.js

--- weframes/src/Form/SearchBar/SearchBar.js
+++ weframes/src/Form/SearchBar/SearchBar.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function SearchBar() {
+function SearchBar({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handleSearch = () => {
+        // You can perform a search operation or pass the search term to a parent component or function.
+        // For now, let's just log the search term to the console.
+        console.log(`Searching for: ${searchTerm}`);
+        if (onSearch) {
+            onSearch(searchTerm);
+        }
+    };
+
     return (
-        <div>SearchBar</div>
-    )
+        <div>
+            <input
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={handleInputChange}
+            />
+            <button onClick={handleSearch}>Search</button>
+        </div>
+    );
 }
 
-export default SearchBar
+export default SearchBar;
