d87e98492f1c4a3d797eb4595c21e2a9b5751126
macosta2015 <macosta2015@my.fit.edu>
2023-10-04 23:56:48 -0400

    Update App.js

--- weframes/src/App.js
+++ weframes/src/App.js
@@ -5,8 +5,14 @@ import Header from './Form/Header/Header';
 import Form from './Form/Form/Form';
 import { FormDataProvider } from './FormDataContext'; // Import the FormDataProvider
 import DisplayData from './Form/Emailoutput/Emailoutput';
+import SearchBar from './Form/SearchBar/SearchBar';
 
 function App() {
+
+  const handleSearch = (searchTerm) => { // Implement your search logic here.
+    console.log(`Searching for: ${searchTerm}`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,7 +20,8 @@ function App() {
         <Form />
         {/* <Card/> */}
         {/* <MediaCard/> */}
-        <DisplayData/>
+        <DisplayData />
+        <SearchBar />
       </header>
     </div>
   );
