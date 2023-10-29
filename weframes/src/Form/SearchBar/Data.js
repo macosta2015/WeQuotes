c542f972eb30227356de8c48df21d4b0f619b188
Acosta Mario <macosta@titanamerica.com>
2023-10-29 19:00:41 -0400

    Update Data.js

--- weframes/src/Form/SearchBar/Data.js
+++ weframes/src/Form/SearchBar/Data.js
@@ -3,6 +3,8 @@ const Data = [
     { id: 1, title: 'Item 1', description: 'Description for Item 1' },
     { id: 2, title: 'Item 2', description: 'Description for Item 2' },
     { id: 3, title: 'Item 3', description: 'Description for Item 3' },
+    { id: 4, title: 'Item 4', description: 'Description for Item 4' },
+
     // Add more items as needed
 ];
 
